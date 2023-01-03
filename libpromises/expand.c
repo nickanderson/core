d839b084c906ccf6e132aa142d4bb6d3703538c2
Nick Anderson <nick@cmdln.org>
2023-01-03 08:57:25 -0600

    Uneducated guess

--- libpromises/expand.c
+++ libpromises/expand.c
@@ -608,7 +608,7 @@ char *ExpandScalar(const EvalContext *ctx, const char *ns, const char *scope,
     BufferDestroy(current_item);
 
     LogDebug(LOG_MOD_EXPAND, "ExpandScalar(%s:%s.%s)  =>  %s",
-             SAFENULL(ns), SAFENULL(scope), varstring, BufferData(out));
+             SAFENULL(ns), SAFENULL(scope), BufferData(current_item), BufferData(out));
 
     return out_belongs_to_us ? BufferClose(out) : BufferGet(out);
 }
